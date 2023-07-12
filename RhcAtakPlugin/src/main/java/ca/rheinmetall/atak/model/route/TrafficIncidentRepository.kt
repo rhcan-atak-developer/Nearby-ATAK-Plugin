bd01446b603a1230ccf292a85a06bc31a3295cd5
Felix Houle <fhoule@rheinmetall.ca>
2023-07-12 15:52:02 -0400

    Remove item in TrafficIncidentRepository when removed on the map

--- RhcAtakPlugin/src/main/java/ca/rheinmetall/atak/model/route/TrafficIncidentRepository.kt
+++ RhcAtakPlugin/src/main/java/ca/rheinmetall/atak/model/route/TrafficIncidentRepository.kt
@@ -1,16 +1,35 @@
 package ca.rheinmetall.atak.model.route
 
 import androidx.lifecycle.MutableLiveData
+import ca.rheinmetall.atak.lifecycle.PluginLifeCycled
+import ca.rheinmetall.atak.model.PointOfInterest
+import com.atakmap.android.maps.MapEvent
+import com.atakmap.android.maps.MapEventDispatcher
+import com.atakmap.android.maps.MapView
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class TrafficIncidentRepository @Inject constructor(){
+class TrafficIncidentRepository @Inject constructor(private val mapView: MapView) : PluginLifeCycled(),
+    MapEventDispatcher.MapEventDispatchListener {
     private val _trafficIncidents: MutableMap<String?, TrafficIncident> = HashMap()
     fun addTrafficIncident(trafficIncident: TrafficIncident) {
          _trafficIncidents[trafficIncident.uuid] = trafficIncident
         trafficIncidents.postValue(_trafficIncidents.values.toList())
     }
 
+    override fun start() {
+        mapView.mapEventDispatcher.addMapEventListener(this)
+    }
+
+    override fun stop() {
+        mapView.mapEventDispatcher.removeMapEventListener(this)
+    }
     val trafficIncidents: MutableLiveData<List<TrafficIncident>> = MutableLiveData(ArrayList())
+
+    override fun onMapEvent(mapEvent: MapEvent?) {
+        if (mapEvent?.type == MapEvent.ITEM_REMOVED) {
+            _trafficIncidents.remove(mapEvent.item.uid)
+        }
+    }
 }
