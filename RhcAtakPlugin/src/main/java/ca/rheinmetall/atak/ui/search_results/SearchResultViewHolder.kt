6e67e382bc5588287964689792f249f291a9c30b
jsfdemer <jsfdemers@rheinmetall.ca>
2023-07-12 13:28:41 -0400

    Add icon in the selection list

--- RhcAtakPlugin/src/main/java/ca/rheinmetall/atak/ui/search_results/SearchResultViewHolder.kt
+++ RhcAtakPlugin/src/main/java/ca/rheinmetall/atak/ui/search_results/SearchResultViewHolder.kt
@@ -3,6 +3,9 @@ package ca.rheinmetall.atak.ui.search_results
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import ca.rheinmetall.atak.DistanceFormatter
 import ca.rheinmetall.atak.databinding.SearchResultViewHolderBinding
+import ca.rheinmetall.atak.model.PointOfInterestType
+import com.atakmap.android.icons.UserIconDatabase
+import com.atakmap.android.maps.MapView
 import java.util.function.Consumer
 
 class SearchResultViewHolder(
@@ -10,11 +13,18 @@ class SearchResultViewHolder(
     private val distanceFormatter: DistanceFormatter,
     private val onClick: Consumer<SelectableSearchResult>
 ) : ViewHolder(binding.root) {
+    private val userIconDatabase = UserIconDatabase.instance(MapView._mapView.context)
+    private val iconset = userIconDatabase.getIconSet("6d781afb-89a6-4c07-b2b9-a89748b6a38f", true, true)
     fun bind(result: SelectableSearchResult) {
         binding.container.setOnClickListener { onClick(result) }
         binding.displayName.text = result.result.displayName
         binding.address.text = result.result.address
         binding.distance.text = distanceFormatter.format(result.distance)
+        val type = PointOfInterestType.values().firstOrNull { it.ids.contains(result.result.entityTypeID) }
+
+        val icon = iconset.getIcon(type?.imageName)
+        val bitmap = UserIconDatabase.instance(MapView._mapView.context).getIconBitmap(icon.id)
+        binding.imageView.setImageBitmap(bitmap)
 
         updateSelectedState(result)
     }
